3f463ca85804ae6dbc8c17e5f029d71495eb14c9
Myakis <myakishevnadrey@gmail.com>
2023-08-02 13:53:13 +0300

    hotfix

--- src/entities/crypto/index.ts
+++ src/entities/crypto/index.ts
@@ -1,4 +1,4 @@
-export { type Crypto, type ProductId } from './model/types'
+export { type Crypto } from './model/types'
 export { cryptoSlice } from './model/slice'
 export { CryptoCard } from './ui/CryptoCard/CryptoCard'
 export { useInfoCryptoQuery } from './api/cryptoApi'
